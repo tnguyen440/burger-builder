e8b5660c7999bb469882dc847aea3ed096a71516
Tien Nguyen <tnguyen440@csc.com>
2018-02-02 11:03:44 +0700

    add backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <React.Fragment>
     <div> toolbar, sidedrawer, backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </React.Fragment>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  }
+ 
   render() {
     const disableInfor = {
       ...this.state.ingredients
@@ -83,7 +87,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <React.Fragment>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
