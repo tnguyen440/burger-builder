4d0eeb13971eaebce686cba59f305c6dead41151
Minh Tien Nguyen <minhtien.swin@gmail.com>
2018-02-20 15:59:08 +0700

    enhance performance

--- src/components/Burger/OrderSummary.js
+++ src/components/Burger/OrderSummary.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../Button/Button';
 
 // import classes from './OrderSummary.css';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  // this could be a functional component, doesn'tgit status have to be a class
+  componentWillUpdate() {
+    console.log('[OrderSummary] componentWillUpdate');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => 
       <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
       </li>);
 
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients: </p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout ?</p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients: </p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout ?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </React.Fragment>
+    )
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
