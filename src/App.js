6fff045254792de730e45b15b9a5526ef164cdc6
Tien Nguyen <tnguyen440@csc.com>
2018-04-02 15:11:01 +0700

    add checkout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   state = {
@@ -18,7 +20,11 @@ class App extends Component {
     return (
       <div className="App">
         <Layout>
-            {this.state.show ? <BurgerBuilder /> : null}
+            {/* {this.state.show ? <BurgerBuilder /> : null} */}
+            <Switch>
+              <Route path='/checkout' component={Checkout} />
+              <Route path='/' exact component={BurgerBuilder} />
+            </Switch>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,30 +91,31 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You Continue')
-    this.setState({ isLoading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Minh Nguyen',
-        address: {
-          street: '1 Infinity Loop',
-          zipCode: '100000',
-          country: 'Vietnam'
-        },
-        email: 'test@gmail.com'
-      },
-      deliverMethod: 'slowest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        // console.log(response);
-        this.setState({ isLoading: false, purchasing: false });
-      })
-      .catch(err => {
-        // console.log(err);
-        this.setState({ isLoading: false, purchasing: false });
-      });
+    // this.setState({ isLoading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Minh Nguyen',
+    //     address: {
+    //       street: '1 Infinity Loop',
+    //       zipCode: '100000',
+    //       country: 'Vietnam'
+    //     },
+    //     email: 'test@gmail.com'
+    //   },
+    //   deliverMethod: 'slowest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     // console.log(response);
+    //     this.setState({ isLoading: false, purchasing: false });
+    //   })
+    //   .catch(err => {
+    //     // console.log(err);
+    //     this.setState({ isLoading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   }
  
   render() {
@@ -144,7 +145,7 @@ class BurgerBuilder extends Component {
           />
          </React.Fragment>
         );
-        orderSummary = <OrderSummary 
+      orderSummary = <OrderSummary 
         ingredients={this.state.ingredients}
         price={this.state.totalPrice}
         purchaseCancelled={this.purchaseCancelHandler}
@@ -158,7 +159,7 @@ class BurgerBuilder extends Component {
     return (
       <React.Fragment>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-        {orderSummary}
+          {orderSummary}
         </Modal>
         {burger}
       </React.Fragment>
