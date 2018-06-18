78791354281c6e24bc016dcf5ced0d77d066a0c5
Tien Nguyen <tien.minh@hubcba.com>
2018-06-18 17:00:27 +0700

    adding a spinner

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,9 @@ import classes from './Auth.css';
 
 import Input from '../../components/UI/Input';
 import Button from '../../components/Button/Button';
+import Spinner from '../../components/UI/Spinner';
 import * as actions from '../../store/actions/index';
+import { stat } from 'fs';
 
 class Auth extends Component {
   state = {
@@ -112,7 +114,7 @@ class Auth extends Component {
       });
     }
 
-    const form = formElementArray.map(formElement => (
+    let form = formElementArray.map(formElement => (
       <Input
         key={formElement.id}
         elementType={formElement.config.elementType}
@@ -126,8 +128,21 @@ class Auth extends Component {
       />
     ));
 
+    if (this.props.loading) {
+      form = <Spinner />
+    }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      );
+    }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">Submit</Button>
@@ -143,10 +158,17 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
